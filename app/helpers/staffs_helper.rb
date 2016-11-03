69ffe15053e2f0d532ae8764538ac99fc2774b58
kengste <koh.kenghong@outlook.com>
2016-11-04 00:51:56 +0800

    fixed welcome page

--- app/helpers/staffs_helper.rb
+++ app/helpers/staffs_helper.rb
@@ -5,7 +5,7 @@ module StaffsHelper
 
   def staff_name(staff)
     if current_page?(staffs_path)
-      link_to staff.first_name, new_staff_appointment_path(staff), :remote => true
+      link_to "#{staff.first_name} #{staff.last_name}", new_staff_appointment_path(staff), :remote => true
     else
       staff.first_name
     end
